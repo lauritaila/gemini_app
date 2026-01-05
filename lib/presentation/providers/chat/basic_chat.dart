f8ab2791850c51c65524f92bc68cb916f130935f
Laura Avila <lauritaila10gmail.com>
2026-01-05 16:15:57 -0400

    FEAT: enhance BasicChat with message response logic and state management integration

--- lib/presentation/providers/chat/basic_chat.dart
+++ lib/presentation/providers/chat/basic_chat.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_chat_types/flutter_chat_types.dart';
+import 'package:gemini_app/presentation/providers/chat/is_gemini_writing.dart';
+import 'package:gemini_app/presentation/providers/users/user_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:uuid/uuid.dart';
 
@@ -14,7 +16,6 @@ class BasicChat extends _$BasicChat {
   void addMessage({required PartialText message, required User user}) {
     // TODO: implement logic of imgs
     _addTextMessage(message, user);
-
   }
 
   void _addTextMessage(PartialText partialText, User author) {
@@ -24,7 +25,22 @@ class BasicChat extends _$BasicChat {
       text: partialText.text,
       createdAt: DateTime.now().millisecondsSinceEpoch,
     );
-    state = [message,...state];
+    state = [message, ...state];
+    _geminiTextResponse(partialText.text);
   }
 
+  void _geminiTextResponse(String prompt) async {
+    final isGeminiWriting = ref.read(isGeminiWritingProvider.notifier);
+    final geminiUser = ref.read(geminiUserProvider);
+    isGeminiWriting.setIsGeminiWriting();
+    await Future.delayed(const Duration(seconds: 2));
+    isGeminiWriting.setNotGeminiWriting();
+    final message = TextMessage(
+      author: geminiUser,
+      id: const Uuid().v4(),
+      text: 'This is a response to: $prompt',
+      createdAt: DateTime.now().millisecondsSinceEpoch,
+    );
+    state = [message, ...state];
+  }
 }
